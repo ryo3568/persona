1ddc2549c4eae229bc4ce44b75f5fb8d1c5a8ac0
ryo3568 <ryoyanagi0813@gmail.com>
2022-08-26 17:47:08 +0900

    first commit

---
+++ src/dataloader.py
@@ -0,0 +1,48 @@
+import torch 
+from torch.utils.data import Dataset 
+from torch.nn.utils.rnn import pad_sequence 
+import pickle 
+import pandas as pd 
+import numpy as np 
+from sklearn.preprocessing import StandardScaler 
+
+class Hazumi1911Dataset(Dataset):
+
+    def __init__(self, test_file, train=True, scaler=None):
+    
+        path = '../data/Hazumi1911/Hazumi1911_features/Hazumi1911_features.pkl'
+
+
+        self.videoIDs, self.videoLabels, self.videoText, self.videoAudio,\
+        self.videoVisual, self.videoSentence, self.Vid = pickle.load(open(path, 'rb'), encoding='utf-8')
+
+        self.keys = [] 
+
+        if train:
+            for x in self.Vid:
+                if x != test_file:
+                    self.keys.append(x) 
+            # self.scaler = standardize()
+            self.scaler = scaler
+        else:
+            self.keys.append(test_file)
+            self.scaler = scaler 
+
+        self.len = len(self.keys) 
+
+        
+    def __getitem__(self, index):
+        vid = self.keys[index] 
+        return torch.FloatTensor(self.videoText[vid]),\
+               torch.FloatTensor(self.videoVisual[vid]),\
+               torch.FloatTensor(self.videoAudio[vid]),\
+               torch.FloatTensor([1]*len(self.videoLabels[vid])),\
+               torch.FloatTensor(self.videoLabels[vid]),\
+               vid
+
+    def __len__(self):
+        return self.len 
+
+    def collate_fn(self, data):
+        dat = pd.DataFrame(data)
+        return [pad_sequence(dat[i], True) if i<5 else dat[i].tolist() for i in dat]
---
+++ src/model.py
@@ -0,0 +1,34 @@
+import math 
+import torch 
+import torch.nn as nn 
+import torch.nn.functional as F 
+from torch.autograd import Variable 
+
+if torch.cuda.is_available():
+    FloatTensor = torch.cuda.FloatTensor
+    LongTensor = torch.cuda.LongTensor
+    ByteTensor = torch.cuda.ByteTensor
+
+else:
+    FloatTensor = torch.FloatTensor
+    LongTensor = torch.LongTensor
+    ByteTensor = torch.ByteTensor
+
+class LSTMModel(nn.Module):
+
+    def __init__(self, D_i, D_h, D_o, n_classes=5, dropout=0.5):
+        
+        super(LSTMModel, self).__init__()
+
+        self.dropout = nn.Dropout(dropout)
+        self.lstm = nn.LSTM(input_size=D_i, hidden_size=D_h, num_layers=2, bidirectional=True, dropout=dropout, batch_first=True)
+
+        self.linear1 = nn.Linear(2*D_h, D_o)
+        self.linear2 = nn.Linear(D_o, n_classes)
+
+    def forward(self, x):
+        h, _ = self.lstm(x)
+        h = F.relu(self.linear1(h[:, -1]))
+        h = self.dropout(h)
+        y = self.linear2(h)
+        return y
